73752971f12720dc68ce97ee09ec0ffa26298f48
joey.barden@gmail.com <joey.barden@gmail.com>
2026-04-28 23:52:54 -0400

    Asteroids added but no collisions or shooting yet

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+                                
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    
     while True:
         log_state()
 
@@ -18,9 +35,14 @@ def main():
                 return
 
         screen.fill("black")
+        updatable.update(dt)
+        
+        for i in drawable:
+            i.draw(screen)
 
-        # always call flip() last
         pygame.display.flip()
+        dt = (game_clock.tick(60)/1000)
+        
 
 if __name__ == "__main__":
     main()
